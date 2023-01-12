e8be3ee7d4b8ce567e983138c3313ad82c2ba684
Calathea-Z <zsykes21@gmail.com>
2023-01-12 16:55:45 -0500

    Fixing Heroku

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const {MONGODB_URI} = process.env
+const {MONGODB_URI} = process.env
 
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI || 4000);
