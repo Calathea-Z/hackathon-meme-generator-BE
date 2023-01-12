e2723c2f602cc2962d496ed7063f6348604c5bb0
Calathea-Z <zsykes21@gmail.com>
2023-01-12 17:03:31 -0500

    test

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const {MONGODB_URI} = process.env
+const mongoose = require("mongoose")
+const { MONGODB_URI } = process.env
 
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGODB_URI || 4000);
+mongoose.set("strictQuery", true)
+mongoose.connect(MONGODB_URI)
 
 mongoose.connection
-  .on("open", () => console.log("You are connected to mongoose"))
-  .on("close", () => console.log("You are disconnected from mongoose"))
-  .on("error", (error) => console.log(error));
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error))
